51dc785ad913282a6c7c4c6d37a049040580a989
ShellyHZX <shellylove1119@gmail.com>
2026-06-04 10:07:21 +1000

    Create RootView.swift

---
+++ Circleu/App/RootView.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+struct RootView: View {
+
+    var body: some View {
+
+        TabView {
+
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+
+            JournalView()
+                .tabItem {
+                    Image(systemName: "mic.fill")
+                    Text("Journal")
+                }
+
+            CircleView()
+                .tabItem {
+                    Image(systemName: "person.3.fill")
+                    Text("Circle")
+                }
+
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Noot")
+                }
+        }
+        .tint(Color.blue)
+    }
+}
+
+#Preview {
+    RootView()
+}
