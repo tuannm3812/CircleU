855c4bcb3ce0fbdfa28ece11c81d81c64a686450
ShellyHZX <shellylove1119@gmail.com>
2026-06-04 10:07:26 +1000

    Create CircleuApp.swift

---
+++ Circleu/App/CircleuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleuApp.swift
+//  Circleu
+//
+//  Created by David Oyarekhua on 2/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct CircleuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
